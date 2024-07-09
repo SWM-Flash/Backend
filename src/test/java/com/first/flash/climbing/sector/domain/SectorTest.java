9b3031205486f32634e3443e74c3777cbc3f04eb
WonyuChoi <phillip07290@naver.com>
2024-07-09 18:18:01 +0900

    FLASH-131 test: 섹터 탈거일 업데이트 기능 테스트

--- src/test/java/com/first/flash/climbing/sector/domain/SectorTest.java
+++ src/test/java/com/first/flash/climbing/sector/domain/SectorTest.java
@@ -1,5 +1,6 @@
 package com.first.flash.climbing.sector.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.first.flash.climbing.sector.exception.exceptions.InvalidRemovalDateException;
@@ -15,6 +16,31 @@ class SectorTest {
         // when & then
         assertThatThrownBy(() -> Sector.createDefault("test", "test", LocalDate.now(),
             LocalDate.now().minusDays(1), DEFAULT_GYM_ID))
+            .isInstanceOf(InvalidRemovalDateException.class);
+    }
+
+    @Test
+    void 탈거일_업데이트() {
+        // given
+        Sector sector = Sector.createExceptRemovalDate("test", "test", LocalDate.now(),
+            DEFAULT_GYM_ID);
+
+        // when
+        sector.updateRemovalDate(LocalDate.now().plusDays(1));
+
+        // then
+        LocalDate removalDate = sector.getRemovalDate();
+        assertThat(removalDate).isEqualTo(LocalDate.now().plusDays(1));
+    }
+
+    @Test
+    void 탈거일_수정_예외처리() {
+        // given
+        Sector sector = Sector.createExceptRemovalDate("test", "test", LocalDate.now(),
+            DEFAULT_GYM_ID);
+
+        // when & then
+        assertThatThrownBy(() -> sector.updateRemovalDate(LocalDate.now().minusDays(1)))
             .isInstanceOf(InvalidRemovalDateException.class);
     }
 }
