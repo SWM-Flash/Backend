408b466633ce2b3b44bf5a36d69fad597029050d
wonyangs <bennyws99@gmail.com>
2024-07-09 19:31:40 +0900

    FLASH-128 chore: 코드 포맷 수정

--- src/main/java/com/first/flash/climbing/gym/application/ClimbingGymService.java
+++ src/main/java/com/first/flash/climbing/gym/application/ClimbingGymService.java
@@ -6,7 +6,6 @@ import com.first.flash.climbing.gym.domian.ClimbingGym;
 import com.first.flash.climbing.gym.domian.ClimbingGymRepository;
 import com.first.flash.climbing.gym.exception.exceptions.ClimbingGymNotFoundException;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +25,7 @@ public class ClimbingGymService {
 
     public ClimbingGym findClimbingGymById(final Long id) {
         return climbingGymRepository.findById(id)
-            .orElseThrow(()-> new ClimbingGymNotFoundException(id));
+            .orElseThrow(() -> new ClimbingGymNotFoundException(id));
     }
 
     public List<ClimbingGymResponseDto> findAllClimbingGyms() {
--- src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymCreateRequestDto.java
+++ src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymCreateRequestDto.java
@@ -4,7 +4,8 @@ import com.first.flash.climbing.gym.domian.ClimbingGym;
 import com.first.flash.climbing.gym.domian.vo.Difficulty;
 import java.util.List;
 
-public record ClimbingGymCreateRequestDto(String gymName, String thumbnailUrl, String mapImageUrl, List<Difficulty> difficulties) {
+public record ClimbingGymCreateRequestDto(String gymName, String thumbnailUrl, String mapImageUrl,
+                                          List<Difficulty> difficulties) {
 
     public ClimbingGym toEntity() {
         return new ClimbingGym(gymName, thumbnailUrl, mapImageUrl, difficulties);
