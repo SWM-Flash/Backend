fe0b7723a383403e0b8b777dd53156deda97076a
wonyangs <bennyws99@gmail.com>
2024-07-09 15:52:19 +0900

    FLASH-140 test: 클라이밍장 난이도 관련 테스트 추가

---
+++ src/test/java/com/first/flash/climbing/gym/domian/ClimbingGymTest.java
@@ -0,0 +1,32 @@
+package com.first.flash.climbing.gym.domian;
+
+import static com.first.flash.climbing.gym.fixture.ClimbingGymFixture.createDefaultGym;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.first.flash.climbing.gym.exception.exceptions.DifficultyNotFoundException;
+import org.junit.jupiter.api.Test;
+
+class ClimbingGymTest {
+
+    private final static String DEFAULT_CLIMBING_DIFFICULTY = "빨강";
+
+    @Test
+    void validateDifficultyName() {
+        // given
+        ClimbingGym gym = createDefaultGym();
+
+        // when & them
+        assertDoesNotThrow(()->gym.getDifficultyByName(DEFAULT_CLIMBING_DIFFICULTY));
+    }
+
+    @Test
+    void validateDifficultyException() {
+        // given
+        ClimbingGym gym = createDefaultGym();
+
+        // when & then
+        assertThatThrownBy(() -> gym.getDifficultyByName("invalid difficulty"))
+            .isInstanceOf(DifficultyNotFoundException.class);
+    }
+}
