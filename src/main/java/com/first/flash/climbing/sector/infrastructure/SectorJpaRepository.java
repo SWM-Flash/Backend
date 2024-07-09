52a585bdef5aea7c0fedfc18d2f87dc87e8b3261
WonyuChoi <phillip07290@naver.com>
2024-07-09 17:07:34 +0900

    FLASH-131 feat: 섹터 저장, 단건 조회에 대한 기본적인 repository 생성

---
+++ src/main/java/com/first/flash/climbing/sector/infrastructure/SectorJpaRepository.java
@@ -0,0 +1,12 @@
+package com.first.flash.climbing.sector.infrastructure;
+
+import com.first.flash.climbing.sector.domain.Sector;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectorJpaRepository extends JpaRepository<Sector, Long> {
+
+    Sector save(final Sector sector);
+
+    Optional<Sector> findById(final Long id);
+}
