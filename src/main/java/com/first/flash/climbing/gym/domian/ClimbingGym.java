1a554cfe5112342aefc7ad05342ce9b4a6ec45c7
wonyangs <bennyws99@gmail.com>
2024-07-09 15:51:17 +0900

    FLASH-140 feat: 난이도 이름으로 검색 기능 구현

--- src/main/java/com/first/flash/climbing/gym/domian/ClimbingGym.java
+++ src/main/java/com/first/flash/climbing/gym/domian/ClimbingGym.java
@@ -1,9 +1,17 @@
 package com.first.flash.climbing.gym.domian;
 
+import com.first.flash.climbing.gym.domian.vo.Difficulty;
+import com.first.flash.climbing.gym.exception.exceptions.DifficultyNotFoundException;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OrderColumn;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,9 +27,25 @@ public class ClimbingGym {
     private String thumbnailUrl;
     private String mapImageUrl;
 
-    public ClimbingGym(final String gymName, final String thumbnailUrl, final String mapImageUrl) {
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "DIFFICULTY",
+        joinColumns = @JoinColumn(name = "GYM_NUMBER")
+    )
+    @OrderColumn(name = "DIFFICULTY_INDEX")
+    private List<Difficulty> difficulties;
+
+    public ClimbingGym(final String gymName, final String thumbnailUrl, final String mapImageUrl,
+        final List<Difficulty> difficulties) {
         this.gymName = gymName;
         this.thumbnailUrl = thumbnailUrl;
         this.mapImageUrl = mapImageUrl;
+        this.difficulties = difficulties;
+    }
+
+    public Difficulty getDifficultyByName(final String difficultyName) {
+        return difficulties.stream()
+            .filter(difficulty -> difficulty.hasSameName(difficultyName))
+            .findAny()
+            .orElseThrow(() -> new DifficultyNotFoundException(difficultyName));
     }
 }
