9fc01c97f3f3f0d69ea3edd1b6b701e9782d08ca
WonyuChoi <phillip07290@naver.com>
2024-07-11 23:14:47 +0900

    FLASH-134 feat: 필터링, 정렬 가능한 problem 여러 건 조회 API 추가

--- src/main/java/com/first/flash/climbing/problem/ui/ProblemController.java
+++ src/main/java/com/first/flash/climbing/problem/ui/ProblemController.java
@@ -1,21 +1,30 @@
 package com.first.flash.climbing.problem.ui;
 
+import com.first.flash.climbing.problem.application.ProblemReadService;
 import com.first.flash.climbing.problem.application.ProblemsSaveService;
 import com.first.flash.climbing.problem.application.dto.ProblemCreateResponseDto;
+import com.first.flash.climbing.problem.application.dto.ProblemsResponse;
 import com.first.flash.climbing.problem.domain.dto.ProblemCreateRequestDto;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ProblemController {
 
+    private final static String DEFAULT_SIZE = "2";
+
     private final ProblemsSaveService problemsSaveService;
+    private final ProblemReadService problemReadService;
 
     @PostMapping("/gyms/{gymId}/sectors/{sectorId}/problems")
     public ResponseEntity<ProblemCreateResponseDto> saveProblems(
@@ -24,5 +33,17 @@ public class ProblemController {
         @RequestBody final ProblemCreateRequestDto requestDto) {
         return ResponseEntity.status(HttpStatus.CREATED)
                              .body(problemsSaveService.saveProblems(gymId, sectorId, requestDto));
+    }
+
+    @GetMapping("/problems")
+    public ResponseEntity<ProblemsResponse> findAllProblems(
+        @RequestParam(name = "last-id", required = false) final UUID lastId,
+        @RequestParam(required = false) final String sort,
+        @RequestParam(defaultValue = DEFAULT_SIZE, required = false) final int size,
+        @RequestParam(required = false) final List<String> difficulty,
+        @RequestParam(required = false) final List<String> sector,
+        @RequestParam(name = "has-solution", required = false) final Boolean hasSolution) {
+        return ResponseEntity.ok(
+            problemReadService.findAll(lastId, sort, size, difficulty, sector, hasSolution));
     }
 }
