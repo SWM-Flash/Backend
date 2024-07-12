1fbecb98e053c11c87dec2a09cae5ea4902114a5
WonyuChoi <phillip07290@naver.com>
2024-07-12 16:06:55 +0900

    FLASH-134 feat: 커서 값 여부로 다음 응답 여부를 meta에 추가

--- src/main/java/com/first/flash/climbing/problem/application/dto/ProblemsResponse.java
+++ src/main/java/com/first/flash/climbing/problem/application/dto/ProblemsResponse.java
@@ -3,16 +3,15 @@ package com.first.flash.climbing.problem.application.dto;
 import com.first.flash.climbing.problem.domain.QueryProblem;
 import java.util.List;
 
-public record ProblemsResponse(List<ProblemResponse> problems, CursorInfo meta) {
+public record ProblemsResponse(List<ProblemResponse> problems, Meta meta) {
 
-    public static ProblemsResponse of(final List<QueryProblem> queryProblems) {
+    public static ProblemsResponse of(
+        final List<QueryProblem> queryProblems, final String nextCursor) {
         if (queryProblems.isEmpty()) {
             return new ProblemsResponse(List.of(), null);
         }
-
         return new ProblemsResponse(queryProblems.stream()
                                                  .map(ProblemResponse::toDto)
-                                                 .toList(),
-            new CursorInfo(queryProblems.get(queryProblems.size() - 1).getId()));
+                                                 .toList(), Meta.from(nextCursor));
     }
 }
