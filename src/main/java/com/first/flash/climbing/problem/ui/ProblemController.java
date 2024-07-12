c2a661b7596cb20930dab16269b571ddea3e0cb8
wonyangs <bennyws99@gmail.com>
2024-07-12 17:33:31 +0900

    FLASH-153 chore: 모든 contoller에 API 설명 추가

--- src/main/java/com/first/flash/climbing/problem/ui/ProblemController.java
+++ src/main/java/com/first/flash/climbing/problem/ui/ProblemController.java
@@ -3,6 +3,10 @@ package com.first.flash.climbing.problem.ui;
 import com.first.flash.climbing.problem.application.ProblemsSaveService;
 import com.first.flash.climbing.problem.application.dto.ProblemCreateResponseDto;
 import com.first.flash.climbing.problem.domain.dto.ProblemCreateRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,18 +15,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "problems", description = "문제 관리 API")
 @RestController
 @RequiredArgsConstructor
 public class ProblemController {
 
     private final ProblemsSaveService problemsSaveService;
 
+    @Operation(summary = "문제 업로드", description = "특정 섹터에 문제 업로드")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "성공적으로 문제를 업로드함"),
+    })
     @PostMapping("/gyms/{gymId}/sectors/{sectorId}/problems")
     public ResponseEntity<ProblemCreateResponseDto> saveProblems(
         @PathVariable("gymId") final Long gymId,
         @PathVariable("sectorId") final Long sectorId,
         @RequestBody final ProblemCreateRequestDto requestDto) {
         return ResponseEntity.status(HttpStatus.CREATED)
-                             .body(problemsSaveService.saveProblems(gymId, sectorId, requestDto));
+            .body(problemsSaveService.saveProblems(gymId, sectorId, requestDto));
     }
 }
--- src/main/java/com/first/flash/climbing/solution/ui/SolutionController.java
+++ src/main/java/com/first/flash/climbing/solution/ui/SolutionController.java
@@ -4,6 +4,10 @@ import com.first.flash.climbing.solution.application.SolutionService;
 import com.first.flash.climbing.solution.application.dto.SolutionResponseDto;
 import com.first.flash.climbing.solution.application.dto.SolutionsResponseDto;
 import com.first.flash.climbing.solution.domain.dto.SolutionCreateRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "solutions", description = "해설 영상 관리 API")
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
@@ -21,6 +26,10 @@ public class SolutionController {
 
     private final SolutionService solutionService;
 
+    @Operation(summary = "해설 영상 조회", description = "특정 문제에 대한 해설 영상 조회")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "성공적으로 해설 영상을 조회함")
+    })
     @GetMapping("problems/{problemId}/solutions")
     public ResponseEntity<SolutionsResponseDto> getSolutions(@PathVariable final Long problemId) {
         SolutionsResponseDto response = solutionService.findAllSolutionsByProblemId(
@@ -29,6 +38,10 @@ public class SolutionController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "해설 영상 업로드", description = "특정 문제에 대한 해설 영상 업로드")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "성공적으로 해설 영상을 업로드함")
+    })
     @PostMapping("problems/{problemId}/solutions")
     public ResponseEntity<SolutionResponseDto> createSolution(@PathVariable final Long problemId,
         @RequestBody final SolutionCreateRequestDto solutionCreateRequestDto) {
