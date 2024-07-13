be67ec62e3664c55e700d8bd0669fe27af24139b
WonyuChoi <phillip07290@naver.com>
2024-07-13 16:22:55 +0900

    FLASH-134 refactor: findAll 메서드 QueryProblem repository로 이동

--- src/main/java/com/first/flash/climbing/problem/domain/ProblemRepository.java
+++ src/main/java/com/first/flash/climbing/problem/domain/ProblemRepository.java
@@ -1,8 +1,5 @@
 package com.first.flash.climbing.problem.domain;
 
-import com.first.flash.climbing.problem.infrastructure.paging.Cursor;
-import com.first.flash.climbing.problem.infrastructure.paging.SortBy;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,7 +8,4 @@ public interface ProblemRepository {
     Problem save(final Problem problem);
 
     Optional<Problem> findById(final UUID id);
-
-    List<QueryProblem> findAll(final Cursor preCursor, final SortBy sortBy, final int size,
-        final Long gymId, final List<String> difficulty, final List<String> sector, final Boolean hasSolution);
 }
--- src/main/java/com/first/flash/climbing/problem/infrastructure/ProblemRepositoryImpl.java
+++ src/main/java/com/first/flash/climbing/problem/infrastructure/ProblemRepositoryImpl.java
@@ -2,10 +2,6 @@ package com.first.flash.climbing.problem.infrastructure;
 
 import com.first.flash.climbing.problem.domain.Problem;
 import com.first.flash.climbing.problem.domain.ProblemRepository;
-import com.first.flash.climbing.problem.domain.QueryProblem;
-import com.first.flash.climbing.problem.infrastructure.paging.Cursor;
-import com.first.flash.climbing.problem.infrastructure.paging.SortBy;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +12,6 @@ import org.springframework.stereotype.Repository;
 public class ProblemRepositoryImpl implements ProblemRepository {
 
     private final ProblemJpaRepository jpaRepository;
-    private final QueryProblemQueryDslRepository queryProblemQueryDslRepository;
 
     @Override
     public Problem save(final Problem problem) {
@@ -26,13 +21,5 @@ public class ProblemRepositoryImpl implements ProblemRepository {
     @Override
     public Optional<Problem> findById(final UUID id) {
         return jpaRepository.findById(id);
-    }
-
-    @Override
-    public List<QueryProblem> findAll(final Cursor prevCursor, final SortBy sortBy, final int size,
-        final Long gymId, final List<String> difficulty, final List<String> sector,
-        final Boolean hasSolution) {
-        return queryProblemQueryDslRepository.findAll(prevCursor, sortBy, size,
-            gymId, difficulty, sector, hasSolution);
     }
 }
