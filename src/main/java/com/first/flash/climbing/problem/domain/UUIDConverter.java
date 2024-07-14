b02f03c475de9f0e29a1b035949445d26268ae86
WonyuChoi <phillip07290@naver.com>
2024-07-14 23:56:57 +0900

    FLASH-160 feat: uuid pk를 변환할 변환기

---
+++ src/main/java/com/first/flash/climbing/problem/domain/UUIDConverter.java
@@ -0,0 +1,33 @@
+package com.first.flash.climbing.problem.domain;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import java.nio.ByteBuffer;
+import java.util.Objects;
+import java.util.UUID;
+
+@Converter
+public class UUIDConverter implements AttributeConverter<UUID, byte[]> {
+
+    @Override
+    public byte[] convertToDatabaseColumn(final UUID uuid) {
+        if (Objects.isNull(uuid)) {
+            return null;
+        }
+        ByteBuffer byteBuffer = ByteBuffer.wrap(new byte[16]);
+        byteBuffer.putLong(uuid.getMostSignificantBits());
+        byteBuffer.putLong(uuid.getLeastSignificantBits());
+        return byteBuffer.array();
+    }
+
+    @Override
+    public UUID convertToEntityAttribute(final byte[] bytes) {
+        if (Objects.isNull(bytes)) {
+            return null;
+        }
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        long mostSigBits = byteBuffer.getLong();
+        long leastSigBits = byteBuffer.getLong();
+        return new UUID(mostSigBits, leastSigBits);
+    }
+}
