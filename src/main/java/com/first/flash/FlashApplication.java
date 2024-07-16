e28556685e73d77c7faa15e71e5ef6cf6c047a33
WonyuChoi <phillip07290@naver.com>
2024-07-16 14:42:08 +0900

    FLASH-162 config: async 처리가 가능하게 설정

--- src/main/java/com/first/flash/FlashApplication.java
+++ src/main/java/com/first/flash/FlashApplication.java
@@ -2,8 +2,10 @@ package com.first.flash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class FlashApplication {
 
     public static void main(String[] args) {
