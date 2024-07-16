1c8a7adc90e25d988f1d25af3383fcfe3a76576f
WonyuChoi <phillip07290@naver.com>
2024-07-16 21:27:15 +0900

    FLASH-162 feat: 섹터가 만료되면 문제 만료 상태도 업데이트

--- src/main/java/com/first/flash/climbing/problem/application/ProblemsService.java
+++ src/main/java/com/first/flash/climbing/problem/application/ProblemsService.java
@@ -1,7 +1,9 @@
 package com.first.flash.climbing.problem.application;
 
+import com.first.flash.climbing.problem.domain.ProblemRepository;
 import com.first.flash.climbing.problem.domain.QueryProblemRepository;
 import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,9 +14,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProblemsService {
 
     private final QueryProblemRepository queryProblemRepository;
+    private final ProblemRepository problemRepository;
 
     @Transactional
     public void changeRemovalDate(final Long sectorId, final LocalDate removalDate) {
         queryProblemRepository.updateRemovalDateBySectorId(sectorId, removalDate);
+    }
+
+    @Transactional
+    public void expireProblems(final List<Long> expiredSectorsIds) {
+        queryProblemRepository.expireSectorsById(expiredSectorsIds);
+        problemRepository.expireSectorsById(expiredSectorsIds);
     }
 }
