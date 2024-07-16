94980be01ff34bd259ae26aa0cea62e617c8db3a
WonyuChoi <phillip07290@naver.com>
2024-07-16 13:52:56 +0900

    FLASH-162 feat: sector의 탈거일이 변경될 때 이벤트 발송

--- src/main/java/com/first/flash/climbing/sector/application/SectorService.java
+++ src/main/java/com/first/flash/climbing/sector/application/SectorService.java
@@ -4,8 +4,11 @@ import com.first.flash.climbing.sector.application.dto.SectorCreateRequestDto;
 import com.first.flash.climbing.sector.application.dto.SectorWriteDetailResponseDto;
 import com.first.flash.climbing.sector.application.dto.SectorUpdateRemovalDateRequestDto;
 import com.first.flash.climbing.sector.domain.Sector;
+import com.first.flash.climbing.sector.domain.SectorRemovalDateUpdatedEvent;
 import com.first.flash.climbing.sector.domain.SectorRepository;
 import com.first.flash.climbing.sector.exception.exceptions.SectorNotFoundException;
+import com.first.flash.global.event.Events;
+import java.time.LocalDate;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -29,7 +32,9 @@ public class SectorService {
     public SectorWriteDetailResponseDto updateSectorRemovalDate(final Long sectorId,
         final SectorUpdateRemovalDateRequestDto sectorUpdateRemovalDateRequestDto) {
         Sector sector = findById(sectorId);
-        sector.updateRemovalDate(sectorUpdateRemovalDateRequestDto.removalDate());
+        LocalDate removalDate = sectorUpdateRemovalDateRequestDto.removalDate();
+        sector.updateRemovalDate(removalDate);
+        Events.raise(SectorRemovalDateUpdatedEvent.of(sectorId, removalDate));
         return SectorWriteDetailResponseDto.toDto(sector);
     }
 
