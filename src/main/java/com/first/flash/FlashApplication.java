1ceec659b6be34fabafedba446b30cf88c4cbe77
WonyuChoi <phillip07290@naver.com>
2024-07-17 09:56:57 +0900

    FLASH-159 config: tz 서울로 설정

--- src/main/java/com/first/flash/FlashApplication.java
+++ src/main/java/com/first/flash/FlashApplication.java
@@ -1,5 +1,7 @@
 package com.first.flash;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -14,4 +16,8 @@ public class FlashApplication {
         SpringApplication.run(FlashApplication.class, args);
     }
 
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
