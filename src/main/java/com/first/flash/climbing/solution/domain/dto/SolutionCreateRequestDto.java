7197128e0702551aacf3a640e41b63b15a15746f
wonyangs <bennyws99@gmail.com>
2024-08-01 15:22:24 +0900

    FLASH-221 feat: 해설 생성 시 리뷰 항목과 인스타그램 Id에 Null이 들어올 수 있도록 수정

--- src/main/java/com/first/flash/climbing/solution/domain/dto/SolutionCreateRequestDto.java
+++ src/main/java/com/first/flash/climbing/solution/domain/dto/SolutionCreateRequestDto.java
@@ -1,14 +1,12 @@
 package com.first.flash.climbing.solution.domain.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record SolutionCreateRequestDto(
     @NotEmpty(message = "비디오 URL은 필수입니다.") String videoUrl,
     @NotEmpty(message = "업로더 정보는 필수입니다.") String uploader,
-    @NotNull(message = "리뷰 항목은 필수입니다.")
     @Size(max = 500, message = "리뷰는 최대 500자까지 가능합니다.") String review,
-    @NotEmpty(message = "인스타그램 ID는 필수입니다.") String instagramId) {
+    String instagramId) {
 
 }
