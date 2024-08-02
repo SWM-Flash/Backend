cbb1c457be068a2933178ab817149a055927f10b
WonyuChoi <phillip07290@naver.com>
2024-08-02 15:46:30 +0900

    FLASH-199 feat: member에서 필요한 도메인 객체 생성

---
+++ src/main/java/com/first/flash/account/member/domain/Gender.java
@@ -0,0 +1,11 @@
+package com.first.flash.account.member.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum Gender {
+
+    MALE("male"), FEMALE("female");
+
+    private String gender;
+}
---
+++ src/main/java/com/first/flash/account/member/domain/Role.java
@@ -0,0 +1,10 @@
+package com.first.flash.account.member.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum Role {
+
+    ROLE_ADMIN("ROLE_ADMIN"), ROLE_USER("ROLE_USER");
+    private String role;
+}
