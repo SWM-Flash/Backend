dc4f19c30846a4fb3e875b00d1651f6135774d09
WonyuChoi <phillip07290@naver.com>
2024-08-02 15:46:48 +0900

    FLASH-199 feat: member에서 필요한 리포지토리 생성

---
+++ src/main/java/com/first/flash/account/member/domain/MemberRepository.java
@@ -0,0 +1,5 @@
+package com.first.flash.account.member.domain;
+
+public interface MemberRepository {
+
+}
---
+++ src/main/java/com/first/flash/account/member/infrastructure/MemberRepositoryImpl.java
@@ -0,0 +1,11 @@
+package com.first.flash.account.member.infrastructure;
+
+import com.first.flash.account.member.domain.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepositoryImpl implements MemberRepository {
+
+}
