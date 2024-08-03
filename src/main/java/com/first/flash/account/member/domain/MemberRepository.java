bb3fa98d7f960a40fc78e72acf6fa3886f7ee3e3
WonyuChoi <phillip07290@naver.com>
2024-08-03 09:26:33 +0900

    FLASH-200 feat: 회원 저장, id로 조회 메서드 추가

--- src/main/java/com/first/flash/account/member/domain/MemberRepository.java
+++ src/main/java/com/first/flash/account/member/domain/MemberRepository.java
@@ -1,5 +1,10 @@
 package com.first.flash.account.member.domain;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface MemberRepository {
 
+    Member save(final Member member);
+    Optional<Member> findById(final UUID id);
 }
