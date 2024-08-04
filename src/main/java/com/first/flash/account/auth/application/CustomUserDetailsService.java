f672d12c6a5b518562a92fac1f5b3989c2fbe26e
WonyuChoi <phillip07290@naver.com>
2024-08-04 11:08:52 +0900

    FLASH-200 FLASH-200 feat: 현재 member에 맞춘 UserDetails 구현체

---
+++ src/main/java/com/first/flash/account/auth/application/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.first.flash.account.auth.application;
+
+import com.first.flash.account.auth.application.dto.CustomUserDetails;
+import com.first.flash.account.member.application.MemberService;
+import com.first.flash.account.member.domain.Member;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberService memberService;
+
+    @Override
+    public UserDetails loadUserByUsername(final String id) throws UsernameNotFoundException {
+        Member foundMember = memberService.findById(UUID.fromString(id));
+        return new CustomUserDetails(foundMember.getId(), foundMember.getRole());
+    }
+}
