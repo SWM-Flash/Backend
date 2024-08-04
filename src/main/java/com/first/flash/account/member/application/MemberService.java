2226fa7002a23af838cf5785369425665b3781b4
WonyuChoi <phillip07290@naver.com>
2024-08-04 14:01:13 +0900

    FLASH-200 feat: 유저 정보 완성 API

--- src/main/java/com/first/flash/account/member/application/MemberService.java
+++ src/main/java/com/first/flash/account/member/application/MemberService.java
@@ -1,14 +1,20 @@
 package com.first.flash.account.member.application;
 
+import com.first.flash.account.member.application.dto.MemberCompleteRegistrationRequest;
+import com.first.flash.account.member.application.dto.MemberCompleteRegistrationResponse;
 import com.first.flash.account.member.domain.Member;
 import com.first.flash.account.member.domain.MemberRepository;
 import com.first.flash.account.member.exception.exceptions.MemberNotFoundException;
+import com.first.flash.account.member.exception.exceptions.NickNameDuplicatedException;
+import com.first.flash.global.util.AuthUtil;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -20,5 +26,23 @@ public class MemberService {
     public Member findById(final UUID uuid) {
         return memberRepository.findById(uuid)
                                .orElseThrow(MemberNotFoundException::new);
+    }
+
+    @Transactional
+    public MemberCompleteRegistrationResponse completeMemberRegistration(
+        final MemberCompleteRegistrationRequest request) {
+        if (memberRepository.existsByNickName(request.nickName())) {
+            throw new NickNameDuplicatedException();
+        }
+        UUID id = AuthUtil.getId();
+        Member member = findById(id);
+        member.completeRegistration(request.nickName(), request.instagramId(), request.height(),
+            request.reach(), request.profileImageUrl());
+        return MemberCompleteRegistrationResponse.toDto(member);
+    }
+
+    public ConfirmNickNameResponse confirmNickName(final ConfirmNickNameRequest request) {
+        boolean isDuplicated = memberRepository.existsByNickName(request.nickName());
+        return new ConfirmNickNameResponse(!isDuplicated);
     }
 }
