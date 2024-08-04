9fad8a256ef3a9fd4819f52b5f2ec7a85e6cdefb
WonyuChoi <phillip07290@naver.com>
2024-08-04 11:08:52 +0900

    FLASH-200 FLASH-200 feat: 현재 member에 맞춘 UserDetails 구현체

---
+++ src/main/java/com/first/flash/account/auth/application/dto/CustomUserDetails.java
@@ -0,0 +1,47 @@
+package com.first.flash.account.auth.application.dto;
+
+import com.first.flash.account.member.domain.Role;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public record CustomUserDetails(UUID id, Role role) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return id.toString();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
