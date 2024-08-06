6b5e6f94da31c008c15d1c5082f0bcdd55a8c8a3
WonyuChoi <phillip07290@naver.com>
2024-08-06 15:53:20 +0900

    FLASH-201 feat: GoogleEmailProvider 구현

--- src/main/java/com/first/flash/account/auth/infrastructure/GoogleEmailProvider.java
+++ src/main/java/com/first/flash/account/auth/infrastructure/GoogleEmailProvider.java
@@ -1,13 +1,62 @@
 package com.first.flash.account.auth.infrastructure;
 
 import com.first.flash.account.auth.domain.EmailProvider;
+import com.first.flash.account.auth.exception.exceptions.EmailRequestFailedException;
+import com.first.flash.account.auth.infrastructure.dto.google.GoogleEmailResponseDto;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
+@RequiredArgsConstructor
 public class GoogleEmailProvider implements EmailProvider {
+
+    private static final String GOOGLE_API_URL = "https://people.googleapis.com/v1/people/me?personFields=emailAddresses";
+    private static final String AUTH_HEADER = "Authorization";
+    private static final String AUTH_HEADER_PREFIX = "Bearer ";
+
+    private final RestTemplate restTemplate;
 
     @Override
     public String getEmail(final String token) {
-        return null;
+        HttpEntity<String> httpEntity = prepareRequest(token);
+        return requestEmail(httpEntity);
+    }
+
+    private String requestEmail(final HttpEntity<String> httpEntity) {
+        try {
+            ResponseEntity<GoogleEmailResponseDto> response = restTemplate.exchange(GOOGLE_API_URL,
+                HttpMethod.GET, httpEntity, GoogleEmailResponseDto.class);
+            return resolveResponse(response);
+        } catch (Exception e) {
+            throw new EmailRequestFailedException(e.getMessage());
+        }
+    }
+
+    private static String resolveResponse(final ResponseEntity<GoogleEmailResponseDto> response) {
+        if (isSuccessfulResponse(response)) {
+            return response.getBody()
+                           .getEmail();
+        } else {
+            throw new EmailRequestFailedException();
+        }
+    }
+
+    private static boolean isSuccessfulResponse(
+        final ResponseEntity<GoogleEmailResponseDto> response) {
+        return response.getStatusCode().equals(HttpStatus.OK) &&
+            !Objects.isNull(response.getBody());
+    }
+
+    private HttpEntity<String> prepareRequest(final String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(AUTH_HEADER, AUTH_HEADER_PREFIX + token);
+        return new HttpEntity<>(headers);
     }
 }
