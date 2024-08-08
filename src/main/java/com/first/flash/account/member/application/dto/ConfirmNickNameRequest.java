15c12373b36da99aa4a33f69c1c8fbc3e25d3c88
WonyuChoi <phillip07290@naver.com>
2024-08-08 17:03:06 +0900

    FLASH-201 feat: 닉네임 관련 요청에 닉네임에 대한 정규식 제한 추가

--- src/main/java/com/first/flash/account/member/application/dto/ConfirmNickNameRequest.java
+++ src/main/java/com/first/flash/account/member/application/dto/ConfirmNickNameRequest.java
@@ -1,7 +1,9 @@
 package com.first.flash.account.member.application.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 
-public record ConfirmNickNameRequest(@NotEmpty(message = "닉네임은 필수입니다.") String nickName) {
+public record ConfirmNickNameRequest(
+    @NotEmpty(message = "닉네임은 필수입니다.") @Pattern(regexp = "^[a-zA-Z0-9가-힣]{1,20}$", message = "닉네임은 1자에서 20자 사이의 한글, 영문, 숫자만 포함할 수 있습니다.") String nickName) {
 
 }
