48b2565b2e6e4144fb8c4c24842bf2726a3f1ac4
WonyuChoi <phillip07290@naver.com>
2024-08-08 16:32:35 +0900

    FLASH-201 feat: 로그인 요청에 대한 유효성 검사

--- src/main/java/com/first/flash/account/auth/ui/AuthController.java
+++ src/main/java/com/first/flash/account/auth/ui/AuthController.java
@@ -3,10 +3,9 @@ package com.first.flash.account.auth.ui;
 import com.first.flash.account.auth.application.AuthService;
 import com.first.flash.account.auth.application.dto.LoginRequestDto;
 import com.first.flash.account.auth.application.dto.LoginResponseDto;
-import com.first.flash.global.util.AuthUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,18 +17,8 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody final LoginRequestDto request) {
+    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody final LoginRequestDto request) {
         LoginResponseDto loginResponse = authService.login(request);
         return ResponseEntity.ok(loginResponse);
-    }
-
-    @GetMapping("/admin/test")
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok(AuthUtil.getId() + " admin test");
-    }
-
-    @GetMapping("/api/test")
-    public ResponseEntity<String> apiTest() {
-        return ResponseEntity.ok(AuthUtil.getId() + " api test");
     }
 }
