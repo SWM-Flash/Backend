3d62d57d3d187fc231dd764c761288d4fbd6f964
WonyuChoi <phillip07290@naver.com>
2024-08-08 14:12:19 +0900

    FLASH-201 feat: apple email provider 구현체

--- src/main/java/com/first/flash/account/auth/infrastructure/AppleEmailProvider.java
+++ src/main/java/com/first/flash/account/auth/infrastructure/AppleEmailProvider.java
@@ -1,13 +1,29 @@
 package com.first.flash.account.auth.infrastructure;
 
 import com.first.flash.account.auth.domain.EmailProvider;
+import com.first.flash.account.auth.infrastructure.dto.apple.ApplePublicKeys;
+import io.jsonwebtoken.Claims;
+import java.security.PublicKey;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AppleEmailProvider implements EmailProvider {
+
+    private static final String CLAIM_SUB = "sub";
+
+    private final AppleTokenParser appleTokenParser;
+    private final ApplePublicKeyGenerator applePublicKeyGenerator;
+    private final AppleClient appleClient;
 
     @Override
     public String getEmail(final String token) {
-        return null;
+        Map<String, String> appleTokenHeader = appleTokenParser.parseHeader(token);
+        ApplePublicKeys applePublicKeys = appleClient.getApplePublicKeys();
+        PublicKey publicKey = applePublicKeyGenerator.generate(appleTokenHeader, applePublicKeys);
+        Claims claims = appleTokenParser.extractClaims(token, publicKey);
+        return claims.get(CLAIM_SUB, String.class);
     }
 }
