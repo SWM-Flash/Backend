6fd4fc0d3dfa0af9cdbf77d2a0d2b45016293084
WonyuChoi <phillip07290@naver.com>
2024-08-08 14:18:15 +0900

    FLASH-201 feat: emailService 클래스명 변경

--- src/main/java/com/first/flash/account/auth/application/SocialService.java
+++ src/main/java/com/first/flash/account/auth/application/SocialService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class EmailService {
+public class SocialService {
 
     private final Map<Provider, EmailProvider> providers;
 
