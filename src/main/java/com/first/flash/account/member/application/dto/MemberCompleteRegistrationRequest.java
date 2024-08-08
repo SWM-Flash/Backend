5e814e250eda2066eee33972c263674133b9644a
WonyuChoi <phillip07290@naver.com>
2024-08-08 17:03:06 +0900

    FLASH-201 feat: 닉네임 관련 요청에 닉네임에 대한 정규식 제한 추가

--- src/main/java/com/first/flash/account/member/application/dto/MemberCompleteRegistrationRequest.java
+++ src/main/java/com/first/flash/account/member/application/dto/MemberCompleteRegistrationRequest.java
@@ -1,9 +1,11 @@
 package com.first.flash.account.member.application.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 
-public record MemberCompleteRegistrationRequest(@NotEmpty(message = "닉네임은 필수입니다.") String nickName, String instagramId,
+public record MemberCompleteRegistrationRequest(@NotEmpty(message = "닉네임은 필수입니다.") @Pattern(regexp = "^[a-zA-Z0-9가-힣]{1,20}$", message = "닉네임은 1자에서 20자 사이의 한글, 영문, 숫자만 포함할 수 있습니다.") String nickName,
+                                                @Pattern(regexp = "^[a-z0-9._]{2,30}$") String instagramId,
                                                 @Positive(message = "키는 0보다 커야 합니다.") Double height,
                                                 @Positive(message = "리치는 0보다 커야 합니다.") Double reach, String profileImageUrl) {
 
