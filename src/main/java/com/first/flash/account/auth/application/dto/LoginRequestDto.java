e41e665396d68b3fdc6cf03cebd67bad829b605e
WonyuChoi <phillip07290@naver.com>
2024-08-10 11:39:39 +0900

    feat: enum 유효성 검사 범용적으로 변경

--- src/main/java/com/first/flash/account/auth/application/dto/LoginRequestDto.java
+++ src/main/java/com/first/flash/account/auth/application/dto/LoginRequestDto.java
@@ -1,9 +1,11 @@
 package com.first.flash.account.auth.application.dto;
 
-import com.first.flash.global.annotation.ValidProvider;
+import com.first.flash.account.auth.domain.Provider;
+import com.first.flash.global.annotation.ValidEnum;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public record LoginRequestDto(@NotEmpty(message = "토큰은 필수입니다.") String token,
-                              @ValidProvider String provider) {
+                              @NotNull(message = "플랫폼은 필수입니다.") @ValidEnum(enumClass = Provider.class) Provider provider) {
 
 }
