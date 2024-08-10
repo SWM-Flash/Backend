e2c71794b0eae91826168c6e86978bf48bd74851
WonyuChoi <phillip07290@naver.com>
2024-08-10 11:34:24 +0900

    feat: 닉네임 같은지 확인하는 메서드 추가

--- src/main/java/com/first/flash/account/member/domain/Member.java
+++ src/main/java/com/first/flash/account/member/domain/Member.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import java.util.Objects;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -38,6 +39,7 @@ public class Member {
                      .id(id)
                      .email(email)
                      .nickName(nickName)
+                     .socialId(socialId)
                      .instagramId(instagramId)
                      .height(height)
                      .reach(reach)
@@ -48,11 +50,20 @@ public class Member {
     }
 
     public void completeRegistration(final String nickName, final String instagramId,
-        final Double height, final Double reach, final String profileImageUrl) {
+        final Double height, final Gender gender, final Double reach,
+        final String profileImageUrl) {
         this.nickName = nickName;
         this.instagramId = instagramId;
         this.height = height;
+        this.gender = gender;
         this.reach = reach;
         this.profileImageUrl = profileImageUrl;
+    }
+
+    public boolean hasSameNickName(final String nickName) {
+        if (Objects.isNull(this.nickName)) {
+            return false;
+        }
+        return this.nickName.equals(nickName);
     }
 }
