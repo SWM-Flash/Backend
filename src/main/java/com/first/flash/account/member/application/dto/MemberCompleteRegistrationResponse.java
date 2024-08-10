c8c543b8c80f40c75a9c92c95144c510798a89d2
WonyuChoi <phillip07290@naver.com>
2024-08-10 11:44:20 +0900

    feat: 정보 완성 응답에 성별 추가

--- src/main/java/com/first/flash/account/member/application/dto/MemberCompleteRegistrationResponse.java
+++ src/main/java/com/first/flash/account/member/application/dto/MemberCompleteRegistrationResponse.java
@@ -1,13 +1,15 @@
 package com.first.flash.account.member.application.dto;
 
+import com.first.flash.account.member.domain.Gender;
 import com.first.flash.account.member.domain.Member;
 
 public record MemberCompleteRegistrationResponse(String nickName, String instagramId, Double height,
-                                                 Double reach, String profileImageUrl) {
+                                                 Gender gender, Double reach,
+                                                 String profileImageUrl) {
 
     public static MemberCompleteRegistrationResponse toDto(final Member member) {
         return new MemberCompleteRegistrationResponse(
             member.getNickName(), member.getInstagramId(),
-            member.getHeight(), member.getReach(), member.getProfileImageUrl());
+            member.getHeight(), member.getGender(), member.getReach(), member.getProfileImageUrl());
     }
 }
