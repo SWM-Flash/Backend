271484efe6612db56f14af10c73c344067303417
WonyuChoi <phillip07290@naver.com>
2024-08-27 13:10:32 +0900

    FLASH-245 feat: 변경한 api에 맞게 dto 변경

--- src/main/java/com/first/flash/account/auth/infrastructure/dto/google/GoogleEmailResponseDto.java
+++ src/main/java/com/first/flash/account/auth/infrastructure/dto/google/GoogleEmailResponseDto.java
@@ -2,17 +2,12 @@ package com.first.flash.account.auth.infrastructure.dto.google;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.first.flash.account.auth.exception.exceptions.SocialRequestFailedException;
-import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record GoogleEmailResponseDto(
-    @JsonProperty("emailAddresses") List<EmailAddress> emailAddresses) {
+    @JsonProperty("email") String email) {
 
     public String getEmail() {
-        return emailAddresses.stream()
-                             .findFirst()
-                             .map(EmailAddress::value)
-                             .orElseThrow(SocialRequestFailedException::new);
+        return email;
     }
 }
