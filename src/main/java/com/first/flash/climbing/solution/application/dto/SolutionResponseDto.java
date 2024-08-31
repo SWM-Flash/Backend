2cb0064c816aee6bba029e30043fee06007ffc69
wonyangs <bennyws99@gmail.com>
2024-08-31 15:59:17 +0900

    FLASH-248 feat: 해설 조회 시 본인 해설 여부 필드 추가

--- src/main/java/com/first/flash/climbing/solution/application/dto/SolutionResponseDto.java
+++ src/main/java/com/first/flash/climbing/solution/application/dto/SolutionResponseDto.java
@@ -3,17 +3,21 @@ package com.first.flash.climbing.solution.application.dto;
 import com.first.flash.climbing.solution.domain.Solution;
 import com.first.flash.climbing.solution.domain.vo.SolutionDetail;
 import com.first.flash.climbing.solution.domain.vo.UploaderDetail;
+import com.first.flash.global.util.AuthUtil;
 import java.util.UUID;
 
 public record SolutionResponseDto(Long id, String uploader, String review, String instagramId,
-                                  String videoUrl, UUID uploaderId) {
+                                  String videoUrl, UUID uploaderId, Boolean isUploader) {
 
     public static SolutionResponseDto toDto(final Solution solution) {
         SolutionDetail solutionDetail = solution.getSolutionDetail();
         UploaderDetail uploaderDetail = solution.getUploaderDetail();
 
+        UUID currentUserId = AuthUtil.getId();
+        boolean isUploader = uploaderDetail.getUploaderId().equals(currentUserId);
+
         return new SolutionResponseDto(solution.getId(), uploaderDetail.getUploader(),
             solutionDetail.getReview(), uploaderDetail.getInstagramId(),
-            solutionDetail.getVideoUrl(), uploaderDetail.getUploaderId());
+            solutionDetail.getVideoUrl(), uploaderDetail.getUploaderId(), isUploader);
     }
 }
