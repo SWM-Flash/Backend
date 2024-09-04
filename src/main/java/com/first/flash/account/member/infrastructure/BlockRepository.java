316f1f989f56349cc84d18602532b72e22002abb
WonyuChoi <phillip07290@naver.com>
2024-09-04 10:33:21 +0900

    FLASH-249 feat: 특정 유저가 차단한 유저들의 id를 반환하는 메서드 추가

--- src/main/java/com/first/flash/account/member/infrastructure/BlockRepository.java
+++ src/main/java/com/first/flash/account/member/infrastructure/BlockRepository.java
@@ -1,11 +1,18 @@
 package com.first.flash.account.member.infrastructure;
 
 import com.first.flash.account.member.domain.MemberBlock;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlockRepository extends JpaRepository<MemberBlock, Long> {
 
     MemberBlock save(final MemberBlock memberBlock);
+
+    @Query("SELECT mb.blocked.id FROM MemberBlock mb WHERE mb.blocker.id = :blockerId")
+    List<UUID> findBlockedMembers(@Param("blockerId") final UUID blockerId);
 }
