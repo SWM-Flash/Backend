1f8754ae70904287035469c317c3e288ff33e9b2
WonyuChoi <phillip07290@naver.com>
2024-09-05 08:33:52 +0900

    FLASH-250 feat: 해설과 문제 조회 모델을 조인해 마이페이지 dto 반환

--- src/main/java/com/first/flash/climbing/solution/infrastructure/SolutionQueryDslRepository.java
+++ src/main/java/com/first/flash/climbing/solution/infrastructure/SolutionQueryDslRepository.java
@@ -1,8 +1,11 @@
 package com.first.flash.climbing.solution.infrastructure;
 
+import static com.first.flash.climbing.problem.domain.QQueryProblem.queryProblem;
 import static com.first.flash.climbing.solution.domain.QSolution.solution;
 
 import com.first.flash.climbing.solution.domain.Solution;
+import com.first.flash.climbing.solution.infrastructure.dto.MySolutionDto;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import java.util.UUID;
@@ -21,5 +24,18 @@ public class SolutionQueryDslRepository {
                               .where(solution.problemId.eq(problemId)
                                                        .and(solution.uploaderDetail.uploaderId
                                                            .notIn(memberIds))).fetch();
+    }
+
+    public List<MySolutionDto> findByUploaderId(final UUID uploaderId) {
+        return jpaQueryFactory.select(Projections.constructor(MySolutionDto.class,
+                                  queryProblem.gymName, queryProblem.sectorName, queryProblem.difficultyName,
+                                  queryProblem.id, solution.id, solution.createdAt
+                              ))
+                              .from(solution)
+                              .innerJoin(queryProblem)
+                              .on(solution.problemId.eq(queryProblem.id))
+                              .fetchJoin()
+                              .where(solution.uploaderDetail.uploaderId.eq(uploaderId))
+                              .fetch();
     }
 }
