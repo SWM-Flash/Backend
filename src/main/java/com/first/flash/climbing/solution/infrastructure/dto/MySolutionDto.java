1dbeb99501a4bed3bc61dc60100210842a11d043
WonyuChoi <phillip07290@naver.com>
2024-09-05 21:09:57 +0900

    FLASH-250 fix: 저장된 타입에 맞게 수정

--- src/main/java/com/first/flash/climbing/solution/infrastructure/dto/MySolutionDto.java
+++ src/main/java/com/first/flash/climbing/solution/infrastructure/dto/MySolutionDto.java
@@ -1,9 +1,10 @@
 package com.first.flash.climbing.solution.infrastructure.dto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record MySolutionDto(Long solutionId, String videoUrl, String gymName, String sectorName,
-                            String review, String difficultyName, LocalDateTime removalDate,
-                            LocalDateTime settingDate, LocalDateTime uploadedAt) {
+                            String review, String difficultyName, LocalDate removalDate,
+                            LocalDate settingDate, LocalDateTime uploadedAt) {
 
 }
