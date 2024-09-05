570a96d65dfa210bb394f9df85117285e73293f6
WonyuChoi <phillip07290@naver.com>
2024-09-05 21:25:06 +0900

    FLASH-250 feat: 마이페이지에서 보이는 해설에 대한 요약 정보로 수정

--- src/main/java/com/first/flash/climbing/solution/infrastructure/dto/MySolutionDto.java
+++ src/main/java/com/first/flash/climbing/solution/infrastructure/dto/MySolutionDto.java
@@ -1,10 +1,9 @@
 package com.first.flash.climbing.solution.infrastructure.dto;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public record MySolutionDto(Long solutionId, String videoUrl, String gymName, String sectorName,
-                            String review, String difficultyName, LocalDate removalDate,
-                            LocalDate settingDate, LocalDateTime uploadedAt) {
+public record MySolutionDto(Long solutionId, String gymName, String sectorName,
+                            String difficultyName, String problemImageUrl,
+                            LocalDateTime uploadedAt) {
 
 }
