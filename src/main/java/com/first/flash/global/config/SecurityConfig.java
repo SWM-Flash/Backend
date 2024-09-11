04219c95ce0270166a597df5a076c7e9e692008b
WonyuChoi <phillip07290@naver.com>
2024-09-11 15:49:40 +0900

    FLASH-258 feat: WEB 역할은 GET 메서드 요청만 가능하도록 설정

--- src/main/java/com/first/flash/global/config/SecurityConfig.java
+++ src/main/java/com/first/flash/global/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.first.flash.global.filter.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -47,7 +48,9 @@ public class SecurityConfig {
                    .authorizeHttpRequests(authorize -> authorize
                        .requestMatchers(AUTH_WHITELIST).permitAll()
                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                       .anyRequest().authenticated()
+                       .requestMatchers(HttpMethod.GET, "/**").hasRole("WEB")
+                       .requestMatchers("/**").hasAnyRole("ADMIN", "USER")
+                       .anyRequest().denyAll()
                    )
                    .addFilterBefore(new JwtAuthFilter(tokenManager, userDetailsService),
                        UsernamePasswordAuthenticationFilter.class)
