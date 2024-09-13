4f7b4190a96a7dc2b7538343759142b3d998c74b
WonyuChoi <phillip07290@naver.com>
2024-09-13 14:08:51 +0900

    FLASH-264 refactor: 마케팅 수신 동의 여부를 관리하는 API를 분리함에 따라 필요 없어진 코드 삭제

--- src/main/java/com/first/flash/account/auth/application/dto/LoginRequestDto.java
+++ src/main/java/com/first/flash/account/auth/application/dto/LoginRequestDto.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record LoginRequestDto(@NotEmpty(message = "토큰은 필수입니다.") String token,
-                              @NotNull(message = "플랫폼은 필수입니다.") @ValidEnum(enumClass = Provider.class) Provider provider,
-                              Boolean hasAgreedToMarketing) {
+                              @NotNull(message = "플랫폼은 필수입니다.") @ValidEnum(enumClass = Provider.class) Provider provider) {
 
 }
