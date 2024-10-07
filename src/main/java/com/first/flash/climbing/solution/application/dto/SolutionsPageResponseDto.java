efc8b082d6ff427d6b49d9cf5b81fc27ce23c89d
WonyuChoi <phillip07290@naver.com>
2024-10-07 20:09:29 +0900

    chore: 페이지네이션 적용 전 이름과 같도록 수정

--- src/main/java/com/first/flash/climbing/solution/application/dto/SolutionsPageResponseDto.java
+++ src/main/java/com/first/flash/climbing/solution/application/dto/SolutionsPageResponseDto.java
@@ -4,7 +4,7 @@ import com.first.flash.climbing.solution.infrastructure.dto.MySolutionDto;
 import com.first.flash.global.paging.Meta;
 import java.util.List;
 
-public record SolutionsPageResponseDto(List<MySolutionDto> solutions, Meta meta) {
+public record SolutionsPageResponseDto(List<MySolutionDto> mySolutions, Meta meta) {
 
     public static SolutionsPageResponseDto of(
         final List<MySolutionDto> solutions, final String nextCursor) {
