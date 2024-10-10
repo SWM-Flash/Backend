156934a89c64c8c7a7f43f052d10db3f3a5b6d52
WonyuChoi <phillip07290@naver.com>
2024-10-10 18:27:12 +0900

    FLASH-270 feat: 해설을 조회했을 때 응답 dto 값 추가

--- src/main/java/com/first/flash/climbing/solution/application/dto/SolutionCommentResponseDto.java
+++ src/main/java/com/first/flash/climbing/solution/application/dto/SolutionCommentResponseDto.java
@@ -1,10 +1,15 @@
 package com.first.flash.climbing.solution.application.dto;
 
 import com.first.flash.climbing.solution.domain.SolutionComment;
+import java.util.UUID;
 
-public record SolutionCommentResponseDto() {
+public record SolutionCommentResponseDto(Long id, String content, UUID commenterId, String nickName,
+                                         String profileImageUrl) {
 
     public static SolutionCommentResponseDto toDto(final SolutionComment solutionComment) {
-        return new SolutionCommentResponseDto();
+        return new SolutionCommentResponseDto(solutionComment.getId(), solutionComment.getContent(),
+            solutionComment.getCommenterDetail().getCommenterId(),
+            solutionComment.getCommenterDetail().getCommenter(),
+            solutionComment.getCommenterDetail().getProfileImageUrl());
     }
 }
