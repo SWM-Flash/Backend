bfd93966001f039dfd619dcbcde9b16b5a7c1467
WonyuChoi <phillip07290@naver.com>
2024-10-10 18:12:52 +0900

    FLASH-270 feat: 단건 조회, 수정, 삭제 기능 추가

--- src/main/java/com/first/flash/climbing/solution/infrastructure/SolutionCommentRepository.java
+++ src/main/java/com/first/flash/climbing/solution/infrastructure/SolutionCommentRepository.java
@@ -2,6 +2,8 @@ package com.first.flash.climbing.solution.infrastructure;
 
 import com.first.flash.climbing.solution.domain.SolutionComment;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,5 +11,13 @@ public interface SolutionCommentRepository {
 
     SolutionComment save(final SolutionComment solutionComment);
 
-    List<SolutionComment> findBySolutionId(Long solutionId);
+    Optional<SolutionComment> findById(final Long id);
+
+    List<SolutionComment> findBySolutionId(final Long solutionId);
+
+    void deleteByCommenterId(final UUID commenterId);
+
+    void updateCommenterInfo(final UUID commenterId, final String nickName, final String profileImageUrl);
+
+    void delete(final SolutionComment comment);
 }
--- src/main/java/com/first/flash/climbing/solution/infrastructure/SolutionCommentRepositoryImpl.java
+++ src/main/java/com/first/flash/climbing/solution/infrastructure/SolutionCommentRepositoryImpl.java
@@ -2,6 +2,8 @@ package com.first.flash.climbing.solution.infrastructure;
 
 import com.first.flash.climbing.solution.domain.SolutionComment;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Repository;
 public class SolutionCommentRepositoryImpl implements SolutionCommentRepository {
 
     private final SolutionCommentJpaRepository solutionCommentJpaRepository;
+    private final SolutionCommentQueryDslRepository solutionCommentQueryDslRepository;
 
     @Override
     public SolutionComment save(final SolutionComment solutionComment) {
@@ -17,7 +20,28 @@ public class SolutionCommentRepositoryImpl implements SolutionCommentRepository 
     }
 
     @Override
+    public Optional<SolutionComment> findById(final Long id) {
+        return solutionCommentJpaRepository.findById(id);
+    }
+
+    @Override
     public List<SolutionComment> findBySolutionId(final Long solutionId) {
         return solutionCommentJpaRepository.findBySolutionId(solutionId);
+    }
+
+    @Override
+    public void deleteByCommenterId(final UUID commenterId) {
+        solutionCommentJpaRepository.deleteByCommenterDetailCommenterId(commenterId);
+    }
+
+    @Override
+    public void updateCommenterInfo(final UUID commenterId, final String nickName,
+        final String profileImageUrl) {
+        solutionCommentQueryDslRepository.updateCommenterInfo(commenterId, nickName, profileImageUrl);
+    }
+
+    @Override
+    public void delete(final SolutionComment comment) {
+        solutionCommentJpaRepository.delete(comment);
     }
 }
