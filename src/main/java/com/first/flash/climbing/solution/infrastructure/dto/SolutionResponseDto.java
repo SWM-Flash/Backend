ce9d2a5a02aae4eaabb93e201d318c1ef8ba41ac
WonyuChoi <phillip07290@naver.com>
2024-10-11 15:58:29 +0900

    FLASH-293 feat: 읽기 작업에 대한 응답들 모두 댓글 수 추가

--- src/main/java/com/first/flash/climbing/solution/infrastructure/dto/SolutionResponseDto.java
+++ src/main/java/com/first/flash/climbing/solution/infrastructure/dto/SolutionResponseDto.java
@@ -1,25 +1,9 @@
 package com.first.flash.climbing.solution.infrastructure.dto;
 
-import com.first.flash.climbing.solution.domain.Solution;
-import com.first.flash.climbing.solution.domain.vo.SolutionDetail;
-import com.first.flash.climbing.solution.domain.vo.UploaderDetail;
-import com.first.flash.global.util.AuthUtil;
 import java.util.UUID;
 
 public record SolutionResponseDto(Long id, String uploader, String review, String instagramId,
                                   String videoUrl, UUID uploaderId, Boolean isUploader,
-                                  String profileImageUrl) {
+                                  String profileImageUrl, Long commentCount) {
 
-    public static SolutionResponseDto toDto(final Solution solution) {
-        SolutionDetail solutionDetail = solution.getSolutionDetail();
-        UploaderDetail uploaderDetail = solution.getUploaderDetail();
-
-        UUID uploaderId = uploaderDetail.getUploaderId();
-        Boolean isUploader = AuthUtil.isSameId(uploaderId);
-
-        return new SolutionResponseDto(solution.getId(), uploaderDetail.getUploader(),
-            solutionDetail.getReview(), uploaderDetail.getInstagramId(),
-            solutionDetail.getVideoUrl(), uploaderDetail.getUploaderId(), isUploader,
-            uploaderDetail.getProfileImageUrl());
-    }
 }
