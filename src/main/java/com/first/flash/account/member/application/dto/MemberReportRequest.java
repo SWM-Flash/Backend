be05af0d89a2daacc5b45ed57b359428f9f46df2
WonyuChoi <phillip07290@naver.com>
2024-10-13 17:20:08 +0900

    feat: 신고 시나리오에 content type 추가

--- src/main/java/com/first/flash/account/member/application/dto/MemberReportRequest.java
+++ src/main/java/com/first/flash/account/member/application/dto/MemberReportRequest.java
@@ -1,7 +1,11 @@
 package com.first.flash.account.member.application.dto;
 
+import com.first.flash.account.member.domain.ContentType;
+import com.first.flash.global.annotation.ValidEnum;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
-public record MemberReportRequest(@NotEmpty(message = "신고 사유는 필수입니다.") String reason) {
+public record MemberReportRequest(@NotEmpty(message = "신고 사유는 필수입니다.") String reason,
+                                  @NotNull(message = "콘텐츠 타입은 필수입니다.") @ValidEnum(enumClass = ContentType.class) ContentType contentType) {
 
 }
