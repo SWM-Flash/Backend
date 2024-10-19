2f544d65bb778fd9ca6f91baf6cafeea3836552c
WonyuChoi <phillip07290@naver.com>
2024-10-19 14:45:38 +0900

    FLASH-307 feat: 신고 컨텐츠 유형을 선택하지 않으면 해설 신고로 간주

--- src/main/java/com/first/flash/account/member/application/dto/MemberReportRequest.java
+++ src/main/java/com/first/flash/account/member/application/dto/MemberReportRequest.java
@@ -3,9 +3,12 @@ package com.first.flash.account.member.application.dto;
 import com.first.flash.account.member.domain.ContentType;
 import com.first.flash.global.annotation.ValidEnum;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 public record MemberReportRequest(@NotEmpty(message = "신고 사유는 필수입니다.") String reason,
-                                  @NotNull(message = "콘텐츠 타입은 필수입니다.") @ValidEnum(enumClass = ContentType.class) ContentType contentType) {
+                                  @ValidEnum(enumClass = ContentType.class) ContentType contentType) {
 
+    public MemberReportRequest(final String reason, final ContentType contentType) {
+        this.reason = reason;
+        this.contentType = contentType != null ? contentType : ContentType.SOLUTION;
+    }
 }
