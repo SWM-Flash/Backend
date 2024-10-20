26f6fbfe150086f308d0514cd417acce975b9d32
WonyuChoi <phillip07290@naver.com>
2024-10-20 11:18:41 +0900

    feat: 키값 소문자로 통일

--- src/main/java/com/first/flash/global/aspect/LoggingAspect.java
+++ src/main/java/com/first/flash/global/aspect/LoggingAspect.java
@@ -32,7 +32,7 @@ public class LoggingAspect {
     @AfterReturning(pointcut = "execution(* com.first.flash.account.auth.application.CustomUserDetailsService.loadUserByUsername(..))", returning = "userDetails")
     public void logUserDetails(final CustomUserDetails userDetails) {
         Map<String, Object> logData = logContext.get();
-        logData.put("Requester", userDetails.getNickName());
+        logData.put("requester", userDetails.getNickName());
         logContext.set(logData);
     }
 
@@ -54,13 +54,13 @@ public class LoggingAspect {
             String className = joinPoint.getTarget().getClass().getSimpleName();
 
             Map<String, Object> logData = logContext.get();
-            logData.put(className + " Method", method.getName());
+            logData.put(className + " method", method.getName());
 
             Object[] args = joinPoint.getArgs();
             if (args.length > 0) {
-                logData.put("Method Parameters", args);
+                logData.put("method param", args);
             } else {
-                logData.put("Method Parameters", "No parameters");
+                logData.put("method params", "no params");
             }
             logContext.set(logData);
         }
@@ -70,7 +70,7 @@ public class LoggingAspect {
     public void logMethodReturn(final JoinPoint joinPoint, final Object result) {
         if (log.isDebugEnabled()) {
             Map<String, Object> logData = logContext.get();
-            logData.put("Method Return Value", result);
+            logData.put("method return value", result);
             logContext.set(logData);
         }
     }
@@ -78,16 +78,17 @@ public class LoggingAspect {
     @AfterReturning(pointcut = "com.first.flash.global.aspect.PointCuts.allController()", returning = "result")
     public void logResponse(final Object result) {
         Map<String, Object> logData = logContext.get();
+        logData.put("type", "log");
 
         if (result instanceof ResponseEntity<?> responseEntity) {
-            logData.put("Response Status", responseEntity.getStatusCode().value());
-            logData.put("Response Body", responseEntity.getBody());
+            logData.put("response status", responseEntity.getStatusCode().value());
+            logData.put("response body", responseEntity.getBody());
         } else {
-            logData.put("Response", result);
+            logData.put("response", result);
         }
 
         try {
-            log.info("Unified Log: {}", objectMapper.writeValueAsString(logData));
+            log.info(objectMapper.writeValueAsString(logData));
         } catch (Exception e) {
             log.error("Error converting log to JSON", e);
         } finally {
@@ -95,109 +96,20 @@ public class LoggingAspect {
         }
     }
 
-    // ExceptionHandler에서 예외 기록
     @AfterThrowing(pointcut = "execution(* com.first.flash..*ExceptionHandler.*(..))", throwing = "exception")
-    public void logException(RuntimeException exception) {
+    public void logException(final RuntimeException exception) {
         Map<String, Object> logData = logContext.get();
 
-        logData.put("Error Class", exception.getClass().getSimpleName());
-        logData.put("Error Message", exception.getMessage());
+        logData.put("type", "error");
+        logData.put("error class", exception.getClass().getSimpleName());
+        logData.put("error message", exception.getMessage());
 
         try {
-            log.error("Unified Log with Exception: {}", objectMapper.writeValueAsString(logData));
+            log.error(objectMapper.writeValueAsString(logData));
         } catch (Exception e) {
             log.error("Error converting log with exception to JSON", e);
         } finally {
             logContext.remove();
         }
     }
-
-//    @Before("com.first.flash.global.aspect.PointCuts.allController()")
-//    public void doBeforeController() {
-//        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
-//            .getRequest();
-//
-//        log.info("======= Controller Start =======");
-//        log.info("Request URI: {}", request.getRequestURI());
-//        log.info("HTTP Method: {}", request.getMethod());
-//
-//        Enumeration<String> headerNames = request.getHeaderNames();
-//        while (headerNames.hasMoreElements()) {
-//            String headerName = headerNames.nextElement();
-//            log.info("Header: {} = {}", headerName, request.getHeader(headerName));
-//        }
-//
-//        Enumeration<String> parameterNames = request.getParameterNames();
-//        while (parameterNames.hasMoreElements()) {
-//            String parameterName = parameterNames.nextElement();
-//            log.info("Parameter: {} = {}", parameterName, request.getParameter(parameterName));
-//        }
-//    }
-//
-//    @Before("com.first.flash.global.aspect.PointCuts.verifiedControllers()")
-//    public void doBeforeVerifiedController() {
-//        log.info("user id: {}", AuthUtil.getId());
-//    }
-//
-//    @AfterReturning(value = "com.first.flash.global.aspect.PointCuts.allController()", returning = "result")
-//    public void afterReturnController(final Object result) {
-//        if (result instanceof ResponseEntity<?> responseEntity) {
-//            int statusCode = responseEntity.getStatusCode().value();
-//            Object responseBody = responseEntity.getBody();
-//
-//            log.info("Response Status Code: {}", statusCode);
-//            if (!Objects.isNull(responseBody)) {
-//                log.info("Response Body: {}", responseBody);
-//            } else {
-//                log.info("no Response Body");
-//            }
-//        } else {
-//            if (!Objects.isNull(result)) {
-//                log.info("Response: {}", result);
-//            } else {
-//                log.info("no Response");
-//            }
-//        }
-//        log.info("======= Controller End =======");
-//    }
-//
-//    @Before("com.first.flash.global.aspect.PointCuts.allService() || com.first.flash.global.aspect.PointCuts.allRepository()")
-//    public void doBefore(final JoinPoint joinPoint) {
-//        Method method = getMethod(joinPoint);
-//        String objectName = joinPoint.getTarget()
-//                                     .getClass()
-//                                     .getSimpleName();
-//        log.info("======= {} Start =======", objectName);
-//        log.info("method name = {}", method.getName());
-//        Object[] args = joinPoint.getArgs();
-//        if (Objects.isNull(args) || args.length == 0) {
-//            log.info("no parameter");
-//            return;
-//        }
-//        for (Object arg : args) {
-//            if (Objects.isNull(arg)) {
-//                continue;
-//            }
-//            log.info("parameter type = {}", arg.getClass().getSimpleName());
-//            log.info("parameter value = {}", arg);
-//        }
-//    }
-//
-//    @AfterReturning(value = "com.first.flash.global.aspect.PointCuts.allService() || com.first.flash.global.aspect.PointCuts.allRepository()", returning = "result")
-//    public void afterReturnLog(final JoinPoint joinPoint, final Object result) {
-//        if (Objects.isNull(result)) {
-//            return;
-//        }
-//        log.info("return type = {}", result.getClass().getSimpleName());
-//        log.info("return value = {}", result);
-//        String objectName = joinPoint.getTarget()
-//                                     .getClass()
-//                                     .getSimpleName();
-//        log.info("======= {} End =======", objectName);
-//    }
-//
-//    private Method getMethod(final JoinPoint joinPoint) {
-//        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-//        return signature.getMethod();
-//    }
 }
