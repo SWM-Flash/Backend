666c22f35d9aa545861395f4876316212916ff1a
wonyangs <bennyws99@gmail.com>
2024-10-24 18:07:53 +0900

    feat: 클라이밍 생성 시 지도 사진 url을 nullable하도록 수정

--- src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymCreateRequestDto.java
+++ src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymCreateRequestDto.java
@@ -10,7 +10,7 @@ import java.util.List;
 public record ClimbingGymCreateRequestDto(
     @NotEmpty(message = "클라이밍장 이름은 필수입니다.") String gymName,
     @NotEmpty(message = "썸네일 URL은 필수입니다.") String thumbnailUrl,
-    @NotEmpty(message = "지도 이미지 URL은 필수입니다.") String mapImageUrl,
+    String mapImageUrl,
     @NotEmpty(message = "일정 이미지 URL은 필수입니다.") String calendarImageUrl,
     @NotEmpty(message = "난이도 정보는 최소 하나 이상이어야 합니다.")
     List<@Valid @NotNull(message = "난이도 정보는 비어있을 수 없습니다.") Difficulty> difficulties) {
