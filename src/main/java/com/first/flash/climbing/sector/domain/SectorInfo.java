d607a9b5534ce3a77197a092467a19cd93f7d649
WonyuChoi <phillip07290@naver.com>
2024-11-23 17:26:30 +0900

    feat: sector info 엔티티 생성

---
+++ src/main/java/com/first/flash/climbing/sector/domain/SectorInfo.java
@@ -0,0 +1,45 @@
+package com.first.flash.climbing.sector.domain;
+
+import com.first.flash.climbing.sector.domain.vo.SectorName;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
+public class SectorInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String selectedImageUrl;
+    private SectorName sectorName;
+    private Long gymId;
+
+    protected SectorInfo(final String selectedImageUrl, final SectorName sectorName,
+        final Long gymId) {
+        this.selectedImageUrl = selectedImageUrl;
+        this.sectorName = sectorName;
+        this.gymId = gymId;
+    }
+
+    public static SectorInfo createDefault(final String sectorName, final String adminSectorName,
+        final Long gymId, final String selectedImageUrl) {
+        return new SectorInfo(selectedImageUrl, SectorName.of(sectorName, adminSectorName), gymId);
+    }
+
+    public void updateSectorInfo(final String sectorName, final String adminSectorName,
+        final Long gymId, final String selectedImageUrl) {
+        this.sectorName = SectorName.of(sectorName, adminSectorName);
+        this.selectedImageUrl = selectedImageUrl;
+        this.gymId = gymId;
+    }
+}
