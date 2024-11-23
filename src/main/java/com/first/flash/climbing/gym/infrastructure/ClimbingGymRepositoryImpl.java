34a9a34eeb672b2107a11bca6151ccc090789930
WonyuChoi <phillip07290@naver.com>
2024-11-23 17:46:37 +0900

    feat: 변경된 sector 정보 dto를 반환하도록 수정

--- src/main/java/com/first/flash/climbing/gym/infrastructure/ClimbingGymRepositoryImpl.java
+++ src/main/java/com/first/flash/climbing/gym/infrastructure/ClimbingGymRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.first.flash.climbing.gym.infrastructure;
 
 import com.first.flash.climbing.gym.domian.ClimbingGym;
 import com.first.flash.climbing.gym.domian.ClimbingGymRepository;
+import com.first.flash.climbing.gym.infrastructure.dto.SectorInfoResponseDto;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -30,7 +31,7 @@ public class ClimbingGymRepositoryImpl implements ClimbingGymRepository {
     }
 
     @Override
-    public List<String> findGymSectorNamesById(final Long id) {
+    public List<SectorInfoResponseDto> findGymSectorNamesById(final Long id) {
         return climbingGymQueryDslRepository.findSortedSectorNamesByGymId(id);
     }
 }
