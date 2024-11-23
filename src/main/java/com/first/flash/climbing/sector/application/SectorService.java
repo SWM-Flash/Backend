c4073649bc8b8fb9c50034f6f5bc18a8c53defa0
WonyuChoi <phillip07290@naver.com>
2024-11-23 18:34:41 +0900

    feat: sector의 고정 정보 생성 로직 추가

--- src/main/java/com/first/flash/climbing/sector/application/SectorService.java
+++ src/main/java/com/first/flash/climbing/sector/application/SectorService.java
@@ -3,11 +3,15 @@ package com.first.flash.climbing.sector.application;
 import com.first.flash.climbing.gym.domian.ClimbingGymIdConfirmRequestedEvent;
 import com.first.flash.climbing.sector.application.dto.SectorCreateRequestDto;
 import com.first.flash.climbing.sector.application.dto.SectorDetailResponseDto;
+import com.first.flash.climbing.sector.application.dto.SectorInfoCreateRequestDto;
+import com.first.flash.climbing.sector.application.dto.SectorInfoDetailResponseDto;
 import com.first.flash.climbing.sector.application.dto.SectorUpdateRemovalDateRequestDto;
 import com.first.flash.climbing.sector.application.dto.SectorUpdateRequestDto;
 import com.first.flash.climbing.sector.application.dto.SectorsDetailResponseDto;
 import com.first.flash.climbing.sector.domain.Sector;
 import com.first.flash.climbing.sector.domain.SectorExpiredEvent;
+import com.first.flash.climbing.sector.domain.SectorInfo;
+import com.first.flash.climbing.sector.domain.SectorInfoRepository;
 import com.first.flash.climbing.sector.domain.SectorInfoUpdatedEvent;
 import com.first.flash.climbing.sector.domain.SectorRemovalDateUpdatedEvent;
 import com.first.flash.climbing.sector.domain.SectorRepository;
@@ -26,6 +30,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class SectorService {
 
     private final SectorRepository sectorRepository;
+    private final SectorInfoRepository sectorInfoRepository;
 
     @Transactional
     public SectorDetailResponseDto saveSector(final Long gymId,
@@ -35,6 +40,15 @@ public class SectorService {
         Events.raise(ClimbingGymIdConfirmRequestedEvent.of(gymId));
 
         return SectorDetailResponseDto.toDto(sectorRepository.save(sector));
+    }
+    
+    @Transactional
+    public SectorInfoDetailResponseDto saveSectorInfo(final Long gymId,
+        final SectorInfoCreateRequestDto createRequestDto) {
+        SectorInfo sectorInfo = SectorInfo.createDefault(createRequestDto.name(),
+            createRequestDto.adminName(), gymId, createRequestDto.selectedImageUrl());
+        Events.raise(ClimbingGymIdConfirmRequestedEvent.of(gymId));
+        return SectorInfoDetailResponseDto.toDto(sectorInfoRepository.save(sectorInfo));
     }
 
     @Transactional
@@ -58,9 +72,9 @@ public class SectorService {
         final Long sectorId,
         final SectorUpdateRequestDto updateRequestDto) {
         Sector foundSector = findById(sectorId);
-        foundSector.updateSector(updateRequestDto.sectorName(), updateRequestDto.adminSectorName(),
-            updateRequestDto.settingDate(),
-            updateRequestDto.removalDate(), updateRequestDto.gymId());
+//        foundSector.updateSector(updateRequestDto.sectorName(), updateRequestDto.adminSectorName(),
+//            updateRequestDto.settingDate(),
+//            updateRequestDto.removalDate(), updateRequestDto.gymId());
         Events.raise(SectorInfoUpdatedEvent.of(foundSector.getId(), updateRequestDto.sectorName(),
             updateRequestDto.settingDate()));
         Events.raise(ClimbingGymIdConfirmRequestedEvent.of(updateRequestDto.gymId()));
@@ -85,13 +99,15 @@ public class SectorService {
     private Sector createSectorByDto(final Long gymId,
         final SectorCreateRequestDto createRequestDto) {
         if (hasNoRemovalDate(createRequestDto)) {
-            return Sector.createExceptRemovalDate(createRequestDto.name(),
-                createRequestDto.adminName(), createRequestDto.settingDate(), gymId);
+//            return Sector.createExceptRemovalDate(createRequestDto.name(),
+//                createRequestDto.adminName(), createRequestDto.settingDate(), gymId);
+            return null;
         }
 
-        return Sector.createDefault(createRequestDto.name(),
-            createRequestDto.adminName(), createRequestDto.settingDate(),
-            createRequestDto.removalDate(), gymId);
+//        return Sector.createDefault(createRequestDto.name(),
+//            createRequestDto.adminName(), createRequestDto.settingDate(),
+//            createRequestDto.removalDate(), gymId);
+        return null;
     }
 
     private static boolean hasNoRemovalDate(final SectorCreateRequestDto createRequestDto) {
---
+++ src/main/java/com/first/flash/climbing/sector/application/dto/SectorInfoCreateRequestDto.java
@@ -0,0 +1,5 @@
+package com.first.flash.climbing.sector.application.dto;
+
+public record SectorInfoCreateRequestDto(String name, String adminName, String selectedImageUrl) {
+
+}
