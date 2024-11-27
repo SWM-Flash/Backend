9013a1a7effb6c050af628901d57d1eaba164730
WonyuChoi <phillip07290@naver.com>
2024-11-27 16:09:57 +0900

    feat: auth util로 직접 member id를 가져오도록 수정

--- src/main/java/com/first/flash/climbing/favorite/application/MemberFavoriteGymService.java
+++ src/main/java/com/first/flash/climbing/favorite/application/MemberFavoriteGymService.java
@@ -3,6 +3,7 @@ package com.first.flash.climbing.favorite.application;
 import com.first.flash.climbing.favorite.application.dto.MemberFavoriteGymResponseDto;
 import com.first.flash.climbing.favorite.domain.MemberFavoriteGym;
 import com.first.flash.climbing.favorite.domain.MemberFavoriteGymRepository;
+import com.first.flash.global.util.AuthUtil;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -14,9 +15,11 @@ public class MemberFavoriteGymService {
 
     private final MemberFavoriteGymRepository memberFavoriteGymRepository;
 
-    public MemberFavoriteGymResponseDto saveMemberFavoriteGym(final UUID memberId, final Long gymId) {
+    public MemberFavoriteGymResponseDto saveMemberFavoriteGym(final Long gymId) {
+        UUID memberId = AuthUtil.getId();
         MemberFavoriteGym memberFavoriteGym = MemberFavoriteGym.createDefault(memberId, gymId);
-        MemberFavoriteGym savedMemberFavoriteGym = memberFavoriteGymRepository.save(memberFavoriteGym);
+        MemberFavoriteGym savedMemberFavoriteGym = memberFavoriteGymRepository.save(
+            memberFavoriteGym);
         return MemberFavoriteGymResponseDto.toDto(savedMemberFavoriteGym);
     }
 
