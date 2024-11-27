d2bda4471f98cf4c0d10a1a34f8b1cb4443c42ed
WonyuChoi <phillip07290@naver.com>
2024-11-27 16:59:08 +0900

    feat: 클라이밍장 응답에 즐겨찾기를 했는지 여부 필드 추가

--- src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymResponseDto.java
+++ src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymResponseDto.java
@@ -1,10 +1,11 @@
 package com.first.flash.climbing.gym.application.dto;
 
 import com.first.flash.climbing.gym.domian.ClimbingGym;
+import java.util.List;
 
-public record ClimbingGymResponseDto(Long id, String gymName, String thumbnailUrl) {
+public record ClimbingGymResponseDto(Long id, String gymName, String thumbnailUrl, boolean isFavorite) {
 
-    public static ClimbingGymResponseDto toDto(final ClimbingGym gym) {
-        return new ClimbingGymResponseDto(gym.getId(), gym.getGymName(), gym.getThumbnailUrl());
+    public static Object toDto(final ClimbingGym gym, final List<Long> favoriteGymIds) {
+        return new ClimbingGymResponseDto(gym.getId(), gym.getGymName(), gym.getThumbnailUrl(), favoriteGymIds.contains(gym.getId()));
     }
 }
