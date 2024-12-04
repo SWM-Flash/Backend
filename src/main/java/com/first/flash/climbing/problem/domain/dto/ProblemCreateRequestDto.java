8ebd9c5e35b128a84d6d0bcf8326ad644d687ba2
wonyangs <bennyws99@gmail.com>
2024-12-04 16:43:27 +0900

    feat: 문제 생성을 일반 유저도 할 수 있도록 Url 수정

--- src/main/java/com/first/flash/climbing/problem/domain/dto/ProblemCreateRequestDto.java
+++ src/main/java/com/first/flash/climbing/problem/domain/dto/ProblemCreateRequestDto.java
@@ -4,10 +4,10 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record ProblemCreateRequestDto(
-    @NotEmpty(message = "이미지 URL은 필수입니다.") String imageUrl,
+    String imageUrl,
+    String imageSource,
+    Long thumbnailSolutionId,
     @NotEmpty(message = "난이도는 필수입니다.") String difficulty,
-    @NotEmpty(message = "이미지 출처는 필수입니다.") String imageSource,
-    @NotNull(message = "썸네일 해설 아이디는 필수입니다.") Long thumbnailSolutionId,
     @NotNull(message = "홀드 아이디는 필수입니다.") Long holdId) {
 
 }
