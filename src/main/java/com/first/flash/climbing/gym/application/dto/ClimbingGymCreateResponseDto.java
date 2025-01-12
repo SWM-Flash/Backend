fa692dd10e482aa43f50732afeb5b014ee56059a
WonyuChoi <phillip07290@naver.com>
2025-01-12 17:03:12 +0900

    FLASH-448 feat: 클라이밍장 지점 생성 dto에 난이도 정보 삭제

--- src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymCreateResponseDto.java
+++ src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymCreateResponseDto.java
@@ -1,14 +1,12 @@
 package com.first.flash.climbing.gym.application.dto;
 
 import com.first.flash.climbing.gym.domian.ClimbingGym;
-import com.first.flash.climbing.gym.domian.vo.Difficulty;
-import java.util.List;
 
 public record ClimbingGymCreateResponseDto(Long id, String gymName, String thumbnailUrl,
-                                           String mapImageUrl, String calendarImageUrl, List<Difficulty> difficulties) {
+                                           String mapImageUrl, String calendarImageUrl) {
 
     public static ClimbingGymCreateResponseDto toDto(final ClimbingGym gym) {
         return new ClimbingGymCreateResponseDto(gym.getId(), gym.getGymName(),
-            gym.getThumbnailUrl(), gym.getMapImageUrl(), gym.getCalendarImageUrl(), gym.getDifficulties());
+            gym.getThumbnailUrl(), gym.getMapImageUrl(), gym.getCalendarImageUrl());
     }
 }
