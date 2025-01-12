a2e950285f3bf0ea7a6c2554801016ec538d8364
WonyuChoi <phillip07290@naver.com>
2025-01-12 15:47:54 +0900

    FLASH-448 feat: 엔티티 변환 메서드 삭제

--- src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymCreateRequestDto.java
+++ src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymCreateRequestDto.java
@@ -1,6 +1,5 @@
 package com.first.flash.climbing.gym.application.dto;
 
-import com.first.flash.climbing.gym.domian.ClimbingGym;
 import jakarta.validation.constraints.NotEmpty;
 
 public record ClimbingGymCreateRequestDto(
@@ -8,8 +7,4 @@ public record ClimbingGymCreateRequestDto(
     @NotEmpty(message = "썸네일 URL은 필수입니다.") String thumbnailUrl,
     String mapImageUrl,
     @NotEmpty(message = "일정 이미지 URL은 필수입니다.") String calendarImageUrl) {
-
-    public ClimbingGym toEntity() {
-        return new ClimbingGym(gymName, thumbnailUrl, mapImageUrl, calendarImageUrl);
-    }
 }
