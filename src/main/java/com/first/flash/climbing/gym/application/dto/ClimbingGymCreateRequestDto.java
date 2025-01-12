5908d6cb1e35739256855d200010aa6835e018b1
WonyuChoi <phillip07290@naver.com>
2025-01-12 15:20:47 +0900

    FLASH-448 feat: 클라이밍장 지점 생성 dto에서 난이도 제외

--- src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymCreateRequestDto.java
+++ src/main/java/com/first/flash/climbing/gym/application/dto/ClimbingGymCreateRequestDto.java
@@ -1,21 +1,15 @@
 package com.first.flash.climbing.gym.application.dto;
 
 import com.first.flash.climbing.gym.domian.ClimbingGym;
-import com.first.flash.climbing.gym.domian.vo.Difficulty;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import java.util.List;
 
 public record ClimbingGymCreateRequestDto(
     @NotEmpty(message = "클라이밍장 이름은 필수입니다.") String gymName,
     @NotEmpty(message = "썸네일 URL은 필수입니다.") String thumbnailUrl,
     String mapImageUrl,
-    @NotEmpty(message = "일정 이미지 URL은 필수입니다.") String calendarImageUrl,
-    @NotEmpty(message = "난이도 정보는 최소 하나 이상이어야 합니다.")
-    List<@Valid @NotNull(message = "난이도 정보는 비어있을 수 없습니다.") Difficulty> difficulties) {
+    @NotEmpty(message = "일정 이미지 URL은 필수입니다.") String calendarImageUrl) {
 
     public ClimbingGym toEntity() {
-        return new ClimbingGym(gymName, thumbnailUrl, mapImageUrl, calendarImageUrl, difficulties);
+        return new ClimbingGym(gymName, thumbnailUrl, mapImageUrl, calendarImageUrl);
     }
 }
