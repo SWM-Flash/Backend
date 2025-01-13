644f269e697e210240747c2f58263e9f22076596
WonyuChoi <phillip07290@naver.com>
2025-01-13 18:46:43 +0900

    FLASH-450 feat: 예외 핸들러 로직 추가

--- src/main/java/com/first/flash/climbing/achievement/exception/AchievementExceptionHandler.java
+++ src/main/java/com/first/flash/climbing/achievement/exception/AchievementExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.first.flash.climbing.achievement.exception;
 
+import com.first.flash.climbing.achievement.exception.exceptions.AchievementAccessDeniedException;
 import com.first.flash.climbing.achievement.exception.exceptions.AchievementLimitExceededException;
+import com.first.flash.climbing.achievement.exception.exceptions.AchievementNotFoundException;
 import com.first.flash.global.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,18 @@ public class AchievementExceptionHandler {
     public ResponseEntity<ErrorResponseDto> handleAchievementLimitExceededException(
         final AchievementLimitExceededException exception) {
         return getResponseWithStatus(HttpStatus.BAD_REQUEST, exception);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponseDto> handleAchievementNotFoundException(
+        final AchievementNotFoundException exception) {
+        return getResponseWithStatus(HttpStatus.NOT_FOUND, exception);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponseDto> handleAchievementAccessDeniedException(
+        final AchievementAccessDeniedException exception) {
+        return getResponseWithStatus(HttpStatus.FORBIDDEN, exception);
     }
 
     private ResponseEntity<ErrorResponseDto> getResponseWithStatus(final HttpStatus httpStatus,
