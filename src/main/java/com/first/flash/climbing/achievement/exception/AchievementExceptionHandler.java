551e696bb4c40927d7ee1306123e986dca29b2d1
WonyuChoi <phillip07290@naver.com>
2025-01-13 18:27:56 +0900

    FLASH-450 feat: 업적 생성 제한 개수 예외 처리

---
+++ src/main/java/com/first/flash/climbing/achievement/exception/AchievementExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.first.flash.climbing.achievement.exception;
+
+import com.first.flash.climbing.achievement.exception.exceptions.AchievementLimitExceededException;
+import com.first.flash.global.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class AchievementExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponseDto> handleAchievementLimitExceededException(
+        final AchievementLimitExceededException exception) {
+        return getResponseWithStatus(HttpStatus.BAD_REQUEST, exception);
+    }
+
+    private ResponseEntity<ErrorResponseDto> getResponseWithStatus(final HttpStatus httpStatus,
+        final RuntimeException exception) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto(exception.getMessage());
+        return ResponseEntity.status(httpStatus)
+                             .body(errorResponse);
+    }
+}
