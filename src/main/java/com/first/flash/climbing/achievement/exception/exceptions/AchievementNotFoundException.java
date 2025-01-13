3d72e98ae4886c055a8c3b4b8e2ed8b0a908b6fd
WonyuChoi <phillip07290@naver.com>
2025-01-13 18:45:25 +0900

    FLASH-450 feat: 업적을 찾지 못했을 때 예외 객체

---
+++ src/main/java/com/first/flash/climbing/achievement/exception/exceptions/AchievementNotFoundException.java
@@ -0,0 +1,8 @@
+package com.first.flash.climbing.achievement.exception.exceptions;
+
+public class AchievementNotFoundException extends RuntimeException {
+
+    public AchievementNotFoundException(final Long id) {
+        super(String.format("아이디가 %s인 업적을 찾을 수 없습니다.", id));
+    }
+}
