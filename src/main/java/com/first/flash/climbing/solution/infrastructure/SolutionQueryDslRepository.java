528102f656cb900a197e4dbf08f0d25d1d0bddff
WonyuChoi <phillip07290@naver.com>
2025-01-14 21:32:49 +0900

    FLASH-453 feat: 회원 해설 조회 시 데이터 묶음으로 반환하도록 수정

--- src/main/java/com/first/flash/climbing/solution/infrastructure/SolutionQueryDslRepository.java
+++ src/main/java/com/first/flash/climbing/solution/infrastructure/SolutionQueryDslRepository.java
@@ -1,18 +1,23 @@
 package com.first.flash.climbing.solution.infrastructure;
 
+import static com.first.flash.climbing.gym.domian.QClimbingGym.climbingGym;
+import static com.first.flash.climbing.problem.domain.QProblem.problem;
 import static com.first.flash.climbing.problem.domain.QQueryProblem.queryProblem;
 import static com.first.flash.climbing.solution.domain.QSolution.solution;
 import static com.first.flash.climbing.solution.domain.QSolutionComment.solutionComment;
 
 import com.first.flash.account.member.domain.Gender;
+import com.first.flash.climbing.solution.application.dto.DifficultyDto;
+import com.first.flash.climbing.solution.application.dto.MySolutionFilter;
+import com.first.flash.climbing.solution.application.dto.UserSolutionGroupDto;
 import com.first.flash.climbing.solution.infrastructure.dto.DetailSolutionDto;
-import com.first.flash.climbing.solution.infrastructure.dto.MySolutionDto;
 import com.first.flash.climbing.solution.infrastructure.dto.SolutionRepositoryResponseDto;
 import com.first.flash.climbing.solution.infrastructure.paging.SolutionCursor;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
@@ -43,26 +48,77 @@ public class SolutionQueryDslRepository {
                               .fetch();
     }
 
-    public List<MySolutionDto> findByUploaderId(final UUID uploaderId,
-        final SolutionCursor prevSolutionCursor, final int size, final Long gymId,
-        final List<String> difficulty) {
-        return jpaQueryFactory.select(Projections.constructor(MySolutionDto.class,
-                                  solution.id, queryProblem.gymName, queryProblem.sectorName,
-                                  queryProblem.difficultyName, solution.solutionDetail.thumbnailImageUrl,
-                                  solutionComment.count(), solution.solutionDetail.solvedDate, solution.updatedAt
-                              ))
-                              .from(solution)
-                              .innerJoin(queryProblem)
-                              .on(solution.problemId.eq(queryProblem.id))
-                              .leftJoin(solutionComment)
-                              .on(solution.id.eq(solutionComment.solution.id))
-                              .where(solution.uploaderDetail.uploaderId.eq(uploaderId),
-                                  inGym(gymId), inDifficulties(difficulty),
-                                  cursorCondition(prevSolutionCursor))
-                              .groupBy(solution.id)
-                              .orderBy(solution.createdAt.desc())
-                              .limit(size)
-                              .fetch();
+    public List<UserSolutionGroupDto> findByUploaderId(final UUID uploaderId,
+        final MySolutionFilter mySolutionFilter, final SolutionCursor prevCursor, final int size) {
+        List<Tuple> groupedData = jpaQueryFactory
+            .select(
+                climbingGym.gymName,
+                solution.solutionDetail.solvedDate
+            )
+            .from(solution)
+            .join(problem).on(solution.problemId.eq(problem.id))
+            .join(climbingGym).on(problem.gymId.eq(climbingGym.id))
+            .where(
+                applyGymNameFilter(mySolutionFilter.gymInfoId()),
+                applyYearMonthFilter(mySolutionFilter.year(), mySolutionFilter.month()),
+                solution.uploaderDetail.uploaderId.eq(uploaderId),
+                cursorCondition(prevCursor)
+            )
+            .groupBy(climbingGym.gymName, solution.solutionDetail.solvedDate)
+            .orderBy(solution.solutionDetail.solvedDate.desc(), climbingGym.gymName.asc())
+            .limit(size)
+            .fetch();
+
+        return getDtoFromGroupedData(groupedData, uploaderId);
+    }
+
+    private List<UserSolutionGroupDto> getDtoFromGroupedData(final List<Tuple> groupedData,
+        final UUID uploaderId) {
+        return groupedData.stream().map(group -> {
+            String gymName = group.get(climbingGym.gymName);
+            LocalDate solvedDate = group.get(solution.solutionDetail.solvedDate);
+
+            List<Tuple> difficultiesWithThumbnails = jpaQueryFactory
+                .select(
+                    problem.difficultyInfo.difficultyName,
+                    solution.id.count(),
+                    solution.solutionDetail.thumbnailImageUrl
+                )
+                .from(solution)
+                .join(problem).on(solution.problemId.eq(problem.id))
+                .join(climbingGym).on(problem.gymId.eq(climbingGym.id))
+                .where(
+                    solution.uploaderDetail.uploaderId.eq(uploaderId),
+                    climbingGym.gymName.eq(gymName),
+                    solution.solutionDetail.solvedDate.eq(solvedDate)
+                )
+                .groupBy(problem.difficultyInfo.difficultyName)
+                .orderBy(problem.difficultyInfo.level.desc())
+                .fetch();
+
+            List<DifficultyDto> difficulties = difficultiesWithThumbnails.stream()
+                                                                         .map(
+                                                                             tuple -> new DifficultyDto(
+                                                                                 tuple.get(
+                                                                                     problem.difficultyInfo.difficultyName),
+                                                                                 tuple.get(
+                                                                                     solution.id.count())
+                                                                             ))
+                                                                         .toList();
+
+            String thumbnailImageUrl = difficultiesWithThumbnails.stream()
+                                                                 .findFirst()
+                                                                 .map(tuple -> tuple.get(
+                                                                     solution.solutionDetail.thumbnailImageUrl))
+                                                                 .orElse(null);
+
+            return new UserSolutionGroupDto(
+                gymName,
+                difficulties,
+                solvedDate,
+                thumbnailImageUrl
+            );
+        }).toList();
     }
 
     public void updateUploaderInfo(final UUID uploaderId, final String nickName,
@@ -104,21 +160,37 @@ public class SolutionQueryDslRepository {
             Objects.isNull(prevSolutionCursor.cursorValue())) {
             return null;
         }
-        return solution.createdAt.before(LocalDateTime.parse(prevSolutionCursor.cursorValue()));
+        return solution.solutionDetail.solvedDate.lt(
+                           LocalDate.parse(prevSolutionCursor.cursorValue()))
+                                                 .or(solution.solutionDetail.solvedDate.eq(
+                                                                 LocalDate.parse(
+                                                                     prevSolutionCursor.cursorValue()))
+                                                                                       .and(
+                                                                                           climbingGym.gymName.lt(
+                                                                                               prevSolutionCursor.gymName())));
     }
 
-    private BooleanExpression inGym(final Long gymId) {
-        if (Objects.isNull(gymId)) {
+    private BooleanExpression applyGymNameFilter(final Long gymInfoId) {
+        if (Objects.isNull(gymInfoId)) {
             return null;
         }
-        return queryProblem.gymId.eq(gymId);
+        return climbingGym.gymInfoId.eq(gymInfoId);
     }
 
-    private BooleanExpression inDifficulties(final List<String> difficulty) {
-        if (Objects.isNull(difficulty)) {
+    private BooleanExpression applyYearMonthFilter(final Integer yearFilter,
+        final Integer monthFilter) {
+        if (yearFilter == null && monthFilter == null) {
             return null;
         }
-        return queryProblem.difficultyName.in(difficulty);
+        BooleanExpression yearCondition =
+            (yearFilter != null) ? solution.solutionDetail.solvedDate.year().eq(yearFilter) : null;
+        BooleanExpression monthCondition =
+            (monthFilter != null) ? solution.solutionDetail.solvedDate.month().eq(monthFilter)
+                : null;
+
+        return yearCondition != null && monthCondition != null
+            ? yearCondition.and(monthCondition)
+            : yearCondition != null ? yearCondition : monthCondition;
     }
 
     private BooleanExpression notInBlockedMembers(final List<UUID> memberIds) {
