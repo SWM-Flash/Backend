4bf83d0584548464ef8d6f2fa5ab23869fae0b0b
WonyuChoi <phillip07290@naver.com>
2025-01-14 16:29:07 +0900

    FLASH-450 feat: id 없이도 예외 객체를 만들 수 있도록 생성자 추가

--- src/main/java/com/first/flash/climbing/achievement/exception/exceptions/AchievementNotFoundException.java
+++ src/main/java/com/first/flash/climbing/achievement/exception/exceptions/AchievementNotFoundException.java
@@ -5,4 +5,8 @@ public class AchievementNotFoundException extends RuntimeException {
     public AchievementNotFoundException(final Long id) {
         super(String.format("아이디가 %s인 업적을 찾을 수 없습니다.", id));
     }
+
+    public AchievementNotFoundException() {
+        super("업적을 찾을 수 없습니다.");
+    }
 }
