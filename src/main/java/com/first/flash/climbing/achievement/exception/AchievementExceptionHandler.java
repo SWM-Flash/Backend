2f516f33cc62fc720034554d06a73a0cff3d0488
WonyuChoi <phillip07290@naver.com>
2025-01-14 16:29:38 +0900

    FLASH-450 feat: controller advice 어노테이션 추가

--- src/main/java/com/first/flash/climbing/achievement/exception/AchievementExceptionHandler.java
+++ src/main/java/com/first/flash/climbing/achievement/exception/AchievementExceptionHandler.java
@@ -6,8 +6,10 @@ import com.first.flash.climbing.achievement.exception.exceptions.AchievementNotF
 import com.first.flash.global.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class AchievementExceptionHandler {
 
     @ExceptionHandler
